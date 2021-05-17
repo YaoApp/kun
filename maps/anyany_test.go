1c839e21b5063411bc829d931b58ca2fc09fc51b
Max <shadow.iqka@qq.com>
2021-05-17 19:50:06 +0800

    Map merge test

--- maps/anyany_test.go
+++ maps/anyany_test.go
@@ -17,8 +17,18 @@ func TestAnyAnyMerge(t *testing.T) {
 	m1.Set("foo1", "bar1")
 
 	m2 := MakeAnyAny()
-	m2.Set("foo2", "bar1")
+	m2.Set("foo2", "bar2")
 
-	m1.Merge(m2)
+	m3 := MakeAnyAnySync()
+	m3.Set("foo3", "bar3")
+
+	m1.Merge(m2, m3)
 	fmt.Printf("%#v\n", m1)
+
+	m3.Merge(m1, m2)
+	m3.Range(func(key, value interface{}) bool {
+		fmt.Printf("%v=%v\n", key, value)
+		return true
+	})
+
 }
